2bb131528969b598348adbd9cf2c9c319ca639c1
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-03-31 17:27:28 -0300

    fix style corrections from shellcheck

--- test-time.sh
+++ test-time.sh
@@ -7,21 +7,21 @@ while true; do
     curl --silent --connect-timeout 5 --output /dev/null portquiz.net:80
     CODE=$?
     
-    if [ ${CODE} -eq 0 ]; then
+    if [[ ${CODE} -eq 0 ]]; then
         CANT_DE_PUERTOS=$((CANT_DE_PUERTOS + 1))
-    elif [ ${CODE} -eq 7 ]; then
+    elif [[ ${CODE} -eq 7 ]]; then
         START_TIME=$(date +%s)  # obtener la hora actual en segundos
-        echo "$START_TIME"
+        echo "${START_TIME}"
         echo "Cantidad de puertos escaneados hasta que me dieron ban: ${CANT_DE_PUERTOS}"
 
         while true; do    
             curl --silent --connect-timeout 3 portquiz.net:80
             CODE=$?
 
-            if [ ${CODE} -eq 0 ]; then
+            if [[ ${CODE} -eq 0 ]]; then
                 END_TIME=$(date +%s)    # obtener la hora actual en segundos
                 TIME_ELAPSED=$((END_TIME - START_TIME))  # calcular el tiempo transcurrido
-                echo "Tiempo que tardaron en desbanearme $TIME_ELAPSED"
+                echo "Tiempo que tardaron en desbanearme ${TIME_ELAPSED}"
                 exit
             fi
         done
