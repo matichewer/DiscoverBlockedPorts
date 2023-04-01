7a9dc45ebc1fa7207ba963aaf716f7f84eab2978
Matias David Schwerdt <schwerdt.matias@gmail.com>
2023-04-01 12:45:31 -0300

    add sort file

--- test-threads.sh
+++ test-threads.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+rm arch*
+
 ejecutar_curl() {
     START_PORT="$1"
     END_PORT=$((START_PORT+28)) # Se cambia el límite superior del rango
-    seq $START_PORT $END_PORT | xargs -I {} -P 29 bash -c 'port=$1; curl_output=$(curl --silent --connect-timeout 3 portquiz.net:$port); if [ $? -eq 0 ]; then echo $port >> archivo1.txt; else echo $port >> archivo2.txt; fi' _ {}
+    seq $START_PORT $END_PORT | xargs -I {} -P 29 bash -c 'port=$1; curl_output=$(curl --silent --connect-timeout 3 portquiz.net:$port); if [ $? -eq 0 ]; then echo $port >> archivo1.txt; else echo "$?,$port" >> archivo2.txt; fi' _ {}
 }
 
 # Llama a la función con el puerto inicial que deseas utilizar
 ejecutar_curl 80
+
+wait
+sort -n -o archivo1.txt archivo1.txt
+sort -n -o archivo2.txt archivo2.txt
