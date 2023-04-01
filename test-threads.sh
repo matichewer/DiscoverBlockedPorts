199d67e6785972f8dbd3d208a5235c912cc2513f
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-04-01 12:52:53 -0300

    save $? on var

--- test-threads.sh
+++ test-threads.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm archivo*
+rm arch*
 
 ejecutar_curl() {
     START_PORT="$1"
     END_PORT=$((START_PORT+28)) # Se cambia el límite superior del rango
-    seq $START_PORT $END_PORT | xargs -I {} -P 29 bash -c 'port=$1; curl_output=$(curl --silent --connect-timeout 3 portquiz.net:$port); if [ $? -eq 0 ]; then echo $port >> archivo1.txt; else echo "$?,${port}" >> archivo2.txt; fi' _
+    seq $START_PORT $END_PORT | xargs -I {} -P 29 bash -c 'port=$1; curl_output=$(curl --silent --connect-timeout 3 portquiz.net:$port); CODE=$?; echo ${CODE}; if [ ${CODE} -eq 0 ]; then echo $port >> archivo1.txt; else echo "$?,$port" >> archivo2.txt; fi' _ {}
 }
 
 # Llama a la función con el puerto inicial que deseas utilizar
