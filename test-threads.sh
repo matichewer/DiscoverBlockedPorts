7276cb7cf4fcbc0393619c4d7c81ca54eeb9e6cf
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-04-01 12:36:07 -0300

    remove timeout var

--- test-threads.sh
+++ test-threads.sh
@@ -2,9 +2,8 @@
 
 ejecutar_curl() {
     START_PORT="$1"
-    END_PORT=$((START_PORT+29))
-    seq $START_PORT $END_PORT | 
-    xargs -I {} -P 29 bash -c 'curl_output=$(curl --silent --connect-timeout "${TIMEOUT}" portquiz.net:{}); if [ $? -eq 0 ]; then echo "{}" >> archivo1.txt; else echo "{}" >> archivo2.txt; fi'
+    END_PORT=$((START_PORT+28)) # Se cambia el límite superior del rango
+    seq $START_PORT $END_PORT | xargs -I {} -P 29 bash -c 'port=$1; curl_output=$(curl --silent --connect-timeout 3 portquiz.net:$port); if [ $? -eq 0 ]; then echo $port >> archivo1.txt; else echo $port >> archivo2.txt; fi' _ {}
 }
 
 # Llama a la función con el puerto inicial que deseas utilizar
