c32db2d6a70c2fa6ef72da6a49703aaf467647f3
Matias David Schwerdt <schwerdt.matias@gmail.com>
2023-04-04 15:08:42 -0300

    working for wifi uns

--- test-ports.sh
+++ test-ports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set timeout for testing each port
-TIMEOUT=5
+TIMEOUT=3
 TIME_SLEEP=11
 
 # Loop through all ports
@@ -12,15 +12,20 @@ for PORT in {1..65535}; do
     while ${FLAG}; do
         
         # Try to connect to the current port
+        echo "curl --silent --connect-timeout ${TIMEOUT} portquiz.net:${PORT}"
         OUTPUT=$(curl --silent --connect-timeout "${TIMEOUT}" portquiz.net:"${PORT}")
         CODE=$?
-        
+        echo "cURL Code: ${CODE}. Port: ${PORT}"
+
         if [[ ${CODE} -eq 7 ]]; then
             echo "cURL Code: ${CODE}. Port: ${PORT}. Wait 1 min..."
             sleep ${TIME_SLEEP}
         elif [[ ${CODE} -eq 0 ]]; then
             # Set flag variable to false to exit the loop
             FLAG=false
+        elif [[ ${CODE} -eq 28 ]]; then
+            FLAG=false
+            echo "cURL Code: ${CODE}. Port: ${PORT}. Next..."
         fi
         
     done
--- test-threads.sh
+++ test-threads.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
-rm arch*
+START_TIME=$(date +%s)  # obtener la hora actual en segundos
 
 ejecutar_curl() {
     START_PORT="$1"
-    END_PORT=$((START_PORT+4)) # Se cambia el límite superior del rango
-    seq $START_PORT $END_PORT | xargs -I {} -P 5 bash -c 'port=$1; curl_output=$(curl --silent --connect-timeout 3 portquiz.net:${port}); CODE=$?; echo "${CODE},${port}"; if [ ${CODE} -eq 0 ]; then echo ${port} >> archivo1.txt; else echo "$?,$port" >> archivo2.txt; fi' _ {}
+    END_PORT="$2" # Se cambia el límite superior del rango
+    CANT_PORTS=$((END_PORT - START_PORT + 1))
+    seq $START_PORT $END_PORT | 
+    xargs -I {} -P ${CANT_PORTS} bash -c \
+        'PORT=$1;
+        curl_output=$(curl --silent --connect-timeout 3 portquiz.net:${PORT});
+        CODE=$?; 
+        #echo "${PORT},${CODE}"; 
+        if [ ${CODE} -eq 0 ]; then 
+            echo "${PORT}" >> ports-curl-code-0.txt; 
+        elif [ ${CODE} -eq 28 ]; then 
+            echo "${PORT}" >> ports-curl-code-28.txt;
+        elif [ ${CODE} -eq 7 ]; then 
+            echo "${PORT}" >> ports-curl-code-7.txt; 
+        else
+            echo "${PORT},${CODE}" >> ports-curl-code-others.txt;
+        fi; \
+        ' _ {}
 }
 
 # Llama a la función con el puerto inicial que deseas utilizar
-ejecutar_curl 80
-echo
-ejecutar_curl 85
-echo
-ejecutar_curl 90
-echo
-ejecutar_curl 95
-echo
-ejecutar_curl 100
-echo
-ejecutar_curl 105
-
+ejecutar_curl 1 65535
 
 wait
-sort -n -o archivo1.txt archivo1.txt
-sort -n -o archivo2.txt archivo2.txt
+END_TIME=$(date +%s)    # obtener la hora actual en segundos
+TIME_ELAPSED=$((END_TIME - START_TIME))  # calcular el tiempo transcurrido
+echo "Tiempo en testear 65535 puertos: ${TIME_ELAPSED} segundos"
+                
+echo
+echo "Ordenando archivos..."
+[ -f "ports-curl-code-0.txt" ] && sort -n -o ports-curl-code-0.txt ports-curl-code-0.txt
+[ -f "ports-curl-code-28.txt" ] && sort -n -o ports-curl-code-28.txt ports-curl-code-28.txt
+[ -f "ports-curl-code-7.txt" ] && sort -n -o ports-curl-code-7.txt ports-curl-code-7.txt
+[ -f "ports-curl-code-others.txt" ] && sort -n -o ports-curl-code-others.txt ports-curl-code-others.txt
